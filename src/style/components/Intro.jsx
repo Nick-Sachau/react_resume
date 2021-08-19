be43cb696593a8931eb663f16f8d0c53a0a890d7
nick sachau <nicksachau156@gmail.com>
2021-08-19 04:42:24 -0700

    pushing files to laptop

---
+++ src/style/components/Intro.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+    const Intro = () => {
+        return (
+            <div>
+                <h2 className="name">Nick Sachau</h2>
+                <p className="animatedText"></p>
+            </div>
+        )
+    }
+ 
+    export default Intro
+ 
